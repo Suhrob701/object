746a86c8bc2d1b3f999ddef6ef3357be022d7211
Suhrob701 <suhrobnurmamatov909@gmail.com>
2024-11-15 21:11:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const reegistor = {};
+reegistor.name = prompt('Ismingizni kiriting');
+reegistor.id = (prompt("Space idingizni kiriting")); 
+reegistor.password = (prompt("Space passwordingizni kiriting")); 
+console.log(reegistor);
